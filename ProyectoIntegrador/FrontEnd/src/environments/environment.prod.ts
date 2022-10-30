19e18c6bcee180caf420bffecca21c49679ec97b
Matias Lorenzo <matilorenzo15@gmail.com>
2022-10-30 15:54:56 -0300

    "Acerca de" Fin
    Terminamos con Acerca de

--- ProyectoIntegrador/FrontEnd/src/environments/environment.prod.ts
+++ ProyectoIntegrador/FrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'frontendmlg',
+    appId: '1:422262171798:web:bf3027daeed2633c833ab7',
+    storageBucket: 'frontendmlg.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyC4OwdGzaDh2MlkgNT3NnOtJ79g2e5Kixo',
+    authDomain: 'frontendmlg.firebaseapp.com',
+    messagingSenderId: '422262171798',
+  },
   production: true
 };
--- ProyectoIntegrador/FrontEnd/src/environments/environment.ts
+++ ProyectoIntegrador/FrontEnd/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'frontendmlg',
+    appId: '1:422262171798:web:bf3027daeed2633c833ab7',
+    storageBucket: 'frontendmlg.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyC4OwdGzaDh2MlkgNT3NnOtJ79g2e5Kixo',
+    authDomain: 'frontendmlg.firebaseapp.com',
+    messagingSenderId: '422262171798',
+  },
   production: false
 };
 
